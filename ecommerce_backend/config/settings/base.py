ff1ff7dc35bf6f34d7a1b876ab77aaf4ace78c51
Student <student@alx.com>
2025-08-10 21:11:16 +0300

    Fix BASE_DIR typo in base settings

--- ecommerce_backend/config/settings/base.py
+++ ecommerce_backend/config/settings/base.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIresolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-in-production')
